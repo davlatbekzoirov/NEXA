157f96ba2e1cec041a1ac0749eb3eac80322e40c
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-29 18:42:59 +0500

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
-    path('profile/', views.profile_view, name='profile'),
-    path('profile/email/change/', views.email_change_request_view, name='email_change_request'),
-    path('profile/email/confirm/', views.email_change_confirm_view, name='email_change_confirm'),
-    path('profile/password/', views.password_change_view, name='password_change'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/email/change/', views.EmailChangeRequestView.as_view(), name='email_change_request'),
+    path('profile/email/confirm/', views.EmailChangeConfirmView.as_view(), name='email_change_confirm'),
+    path('profile/password/', views.PasswordChangeView.as_view(), name='password_change'),
 ]
