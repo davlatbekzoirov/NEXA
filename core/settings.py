63633b5abf511d26778c55649d45b2eb4ad7bdd8
davlatbekzoirov <davlatbekzoirov08@gmail.com>
2026-07-31 16:37:05 +0500

    update settings

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True
 LOGIN_URL = 'login'
